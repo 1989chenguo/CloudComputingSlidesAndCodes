0f1653e3fbd2deda520dbb4df5bd6af26c4104e4
Guo Chen <1989chenguo@163.com>
2022-04-10 21:52:47 +0800

    Add multi-thread current webserver

---
+++ CodeInSlides/chapter7/HighConcurrent-client.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <pthread.h>
+#include <string.h>
+#include <unistd.h>
+
+#define MAXIN 256
+#define MAXOUT 256
+
+void* client(void* args) {
+  int* sockfdAddr=(int *) args;
+  int sockfd=*sockfdAddr;
+  int n;
+  char sndbuf[MAXIN]; char rcvbuf[MAXOUT];
+  memset(sndbuf,0,MAXIN); 
+  memcpy(sndbuf,"Hello\n",sizeof("Hello\n"));
+  int sleepseconds=rand()%100+100; //Sleep 10s~20s to emulate user input 
+  sleep(sleepseconds);
+  n=write(sockfd, sndbuf, strlen(sndbuf)); /* send */
+  if (n <= 0) {
+    perror("Error: write to socket");
+    close(sockfd);
+    free(sockfdAddr);
+    return NULL;
+  }
+  
+  memset(rcvbuf,0,MAXOUT);               /* clear */
+  n=read(sockfd, rcvbuf, MAXOUT-1);      /* receive */
+  if (n <= 0) {
+    perror("Error: read from socket");
+    close(sockfd);
+    free(sockfdAddr);
+    return NULL;
+  }
+   
+  close(sockfd);
+  free(sockfdAddr);
+  return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+  if(argc!=4)
+  {
+    printf("usage: %s destIP port MAX_CONN_NUM\n",argv[0]);
+    exit(1);
+  }
+  int len;
+  struct in_addr server_addr;
+  if(!inet_aton(argv[1], &server_addr)) 
+    perror("inet_aton");
+  struct sockaddr_in remote_addr;
+  char buf[BUFSIZ];
+  memset(&remote_addr,0,sizeof(remote_addr));
+  remote_addr.sin_family=AF_INET;
+  remote_addr.sin_addr=server_addr;
+  int port=atoi(argv[2]);
+  remote_addr.sin_port=htons(port);
+  int MAX_CONN_NUM=atoi(argv[3]);
+
+
+  srand(time(NULL));
+  for(int i=0;i<MAX_CONN_NUM;i++)
+  {
+    int *client_sockfd=malloc(sizeof(int));
+    if((*client_sockfd=socket(AF_INET,SOCK_STREAM,0))<0)
+    {
+      perror("socket");
+      return 1;
+    }
+
+    if(connect(*client_sockfd,(const struct sockaddr *)&remote_addr,sizeof(remote_addr))<0)
+    {
+      perror("connect");
+      return 1;
+    }
+    struct sockaddr_in localAddress;
+    socklen_t addressLength = sizeof(localAddress);
+    getsockname(*client_sockfd, (struct sockaddr*)&localAddress, &addressLength);
+
+    printf("I'm %s:%d \t connected to server %s:%d\n"
+      ,inet_ntoa( localAddress.sin_addr),ntohs(localAddress.sin_port)
+      ,inet_ntoa(remote_addr.sin_addr),ntohs(remote_addr.sin_port));
+
+    pthread_t th;
+    if(pthread_create(&th, NULL, client, client_sockfd)!=0)
+    {
+      perror("pthread_create failed");
+      exit(1);
+    }
+  }
+  
+  return 0;
+}
---
+++ CodeInSlides/chapter7/HighConcurrent-normal-server.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <pthread.h>
+#include <string.h>
+#include <unistd.h>
+
+#define MAXREQ 256
+
+void* server(void* args) {
+  int* consockfdAddr=(int *) args; 
+  int consockfd=*consockfdAddr;
+  char reqbuf[MAXREQ];
+  int n;
+                    
+  memset(reqbuf,0, MAXREQ);
+  n = read(consockfd,reqbuf,MAXREQ-1); /* Recv request*/
+  if (n <= 0) {
+    perror("Error: read from socket");
+    close(consockfd);
+    free(consockfdAddr);
+    return NULL;
+  }
+  n = write(consockfd, reqbuf, strlen(reqbuf)); /* echo as response*/
+  if (n <= 0) {
+    perror("Error: write to socket");
+    close(consockfd);
+    free(consockfdAddr);
+    return NULL;
+  }
+
+  close(consockfd);
+  free(consockfdAddr);
+  return NULL;
+}
+
+
+int main(int argc, char *argv[])
+{
+  if(argc!=2)
+  {
+    printf("usage: %s port\n",argv[0]);
+    return 1;
+  }
+  int port=atoi(argv[1]);
+
+  struct sockaddr_in my_addr; 
+  my_addr.sin_family=AF_INET;
+  my_addr.sin_addr.s_addr=INADDR_ANY;
+  my_addr.sin_port=htons(port);
+
+  int server_sockfd;
+  if((server_sockfd=socket(AF_INET,SOCK_STREAM,0))<0)
+  {  
+    perror("socket");
+    return 1;
+  }
+
+  int on=1;  
+  if((setsockopt(server_sockfd,SOL_SOCKET,SO_REUSEADDR,&on,sizeof(on)))<0)  
+  {  
+      perror("setsockopt failed");  
+      return 1;  
+  }  
+  
+  if (bind(server_sockfd,(struct sockaddr *)&my_addr,sizeof(my_addr))<0)
+  {
+    perror("bind");
+    return 1;
+  }
+
+  int sin_size=sizeof(struct sockaddr_in);
+  struct sockaddr_in remote_addr;
+  if(listen(server_sockfd,5)<0)
+  {
+    perror("listen");
+    return 1;
+  }
+
+  int inComeConnNum=0;
+  while (1) {
+    int *client_sockfd=malloc(sizeof(int));
+    if((*client_sockfd=accept(server_sockfd,(struct sockaddr *)&remote_addr,&sin_size))<0)
+    {
+      perror("Error: accept");
+      return 1;
+    }
+    inComeConnNum++;
+    printf("[%d connections accepted] from client %s:%d\n",inComeConnNum,inet_ntoa(remote_addr.sin_addr),ntohs(remote_addr.sin_port));
+
+    pthread_t th;
+    if(pthread_create(&th, NULL, server, client_sockfd)!=0)
+    {
+      perror("pthread_create failed");
+      exit(1);
+    }
+
+  }
+  close(server_sockfd);
+
+  return 0;
+}
